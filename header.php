ece446b2864e235e587b06f36fb36e21eeb1c4e8
bhaktithaker <52991649+bhaktithaker@users.noreply.github.com>
2020-02-26 16:55:25 +0100

    Update header.php

--- header.php
+++ header.php
@@ -224,33 +224,6 @@
               </li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a href="EventBooker.php" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Add Event Booking
-                <!-- <span class="right badge badge-danger">New</span> -->
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="event_book_tbl.php" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Event Booking Table
-                <!-- <span class="right badge badge-danger">New</span> -->
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="flat_allot_tbl.php" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Flat Allotment Table
-                <!-- <span class="right badge badge-danger">New</span> -->
-              </p>
-            </a>
-          </li>
            <li class="nav-item">
             <a href="user_reg.php" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
