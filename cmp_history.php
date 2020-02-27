eaac567ede816685ff6bab27f5ae085f3e191b09
Hiren Rathod <36228225+hrathod5005@users.noreply.github.com>
2020-02-27 14:47:59 +0530

    llo

--- cmp_history.php
+++ cmp_history.php
@@ -22,7 +22,7 @@
       	<div class="col-lg-12">
 					<div class="content-panel">
 						<section id="unseen">
-							<table class="table table-bordered table-striped table-condensed">
+							<table class="table table-bordered table-striped table-condensed display" id="cmphisttable" width="100%">
 								<thead>
 									<tr style="text-align: center">
 										<th style="text-align: center">Complaint Number</th>
@@ -80,3 +80,13 @@
 
 
 <?php include('footer.php'); ?>
+<script type="text/javascript">
+	$(document).ready( function () {
+		$('#cmphisttable').DataTable({			        
+				
+			buttons: [ 'copy', 'csv', 'excel', 'pdf', 'print'  ],
+			dom: 'lBfrtip',
+			"lengthChange": true
+		});
+	});
+</script>
--- maintenance_bill_history.php
+++ maintenance_bill_history.php
@@ -17,40 +17,41 @@
     <!-- Main content -->
     <section class="content-header">
       <div class="container-fluid">
-         <table class="table display" id="cattable" width="100%">
+         <table class="table display" id="main_tbl" width="100%">
       <thead>
         <!-- <tr style="text-align: center;"><th colspan="5"><h2>Flat Allotment List</h2></th></tr> -->
         <tr><th>flat no</th><th>user name</th><th>contact no</th><th>bill-date</th><th>total bill</th><th>due date</th><th>view</th></tr>
       </thead>
       <tbody>      
         <?php
-              $result=mysqli_query($con,"select fid,block,flat_num,uid,fullName,contactNo from flat,users where uid IS NOT NULL and uid=id
+              // $result=mysqli_query($con,"select fid,block,flat_num,uid,fullName,contactNo from flat,users where uid IS NOT NULL and uid=id
+//"); 
+              $result=mysqli_query($con,"select flat.fid,block,flat_num,uid,fullName,contactNo,bill_date,water_charges,property_tax,elec_charges,parking_charges,other,flat_charges,due_date,bid from users,maintenance_bill,flat where id=maintenance_bill.fid and uid=id
 "); 
-        $result1=mysqli_query($con,"select fullName,contactNo,bill_date,water_charges,property_tax,elec_charges,parking_charges,other,flat_charges,due_date,bid from users,maintenance_bill where id=fid");           
+        // $result1=mysqli_query($con,"select fullName,fid,block,flat_num,uid,contactNo,bill_date,water_charges,property_tax,elec_charges,parking_charges,other,flat_charges,due_date,bid from users,maintenance_bill where id=fid");           
               // $row=$result->fetch_assoc(); 
-         $dummy = mysqli_num_rows($result1); 
+         // $dummy = mysqli_num_rows($result1); 
          while($row=mysqli_fetch_assoc($result)):; 
                                 // print_r($row); 
                               
         
 
           ?>
-          <td rowspan="<?php echo($dummy);  ?>"><?php //printf("%s",$row['fid']); 
+          <td ><?php //printf("%s",$row['fid']); 
                          echo(($row["block"]." - ".$row["flat_num"])); ?>
                             </td>
-                            <td rowspan="<?php echo($dummy);  ?>"><?php //printf("%s",$row['fid']); 
+                            <td ><?php //printf("%s",$row['fid']); 
                          echo(($row["fullName"])); ?>
                             </td>
-                            <td rowspan="<?php echo($dummy);  ?>"><?php //printf("%s",$row['fid']); 
+                            <td ><?php //printf("%s",$row['fid']); 
                          echo(($row["contactNo"])); ?>
                             </td>
-                                <?php
+          <?php
                         
               // $row=$result->fetch_assoc(); 
-              while($row=mysqli_fetch_assoc($result1)):;  ?>
-                                
-                        <!-- <td rowspan="<?php //echo($dummy);   printf("%s",$row['fullName']);?></td> -->
-                                <!-- <td ><?php// printf("%s",$row['contactNo']);?></td> -->
+              // print_r($result1);
+              // while($row=mysqli_fetch_assoc($result1)):;  ?>
+                                                        
                          <td><?php echo($row['bill_date']);?></td>
                              <td><?php $total=$row['property_tax']+$row['water_charges']+$row['flat_charges']+$row['parking_charges']+$row['other']+$row['elec_charges'];
                               echo($total);?></td>
@@ -59,7 +60,7 @@
                         </tr>
             <!-- <td></td> -->
               <?php endwhile;?>
-                        <?php endwhile;?>
+         <?php //endwhile;?>
                     </table>
       
         
@@ -72,4 +73,13 @@
   <!-- /.content-wrapper -->
 
 <?php include('footer.php') ?>
-
+<script type="text/javascript">
+  $(document).ready( function () {
+    $('#main_tbl').DataTable({              
+        
+      buttons: [ 'copy', 'csv', 'excel', 'pdf', 'print'  ],
+      dom: 'lBfrtip',
+      "lengthChange": true
+    });
+  });
+</script>
