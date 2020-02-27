e59cfb8b60f22b8dd4431f9ed0c14f4a983211ee
Hiren Rathod <36228225+hrathod5005@users.noreply.github.com>
2020-02-27 21:53:25 +0530

    pp

--- admin/header.php
+++ admin/header.php
@@ -81,7 +81,7 @@ else{
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link">
+    <a href="index.php" class="brand-link">
       <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">SMS</span>
@@ -95,7 +95,7 @@ else{
           <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Admin</a>
+          <a href="#" class="d-block">Admin / Member</a>
         </div>
       </div>
 
@@ -379,7 +379,7 @@ else{
             </ul>
           </li>
            <li class="nav-item has-treeview">
-            <a href="logout.php" class="nav-link">
+            <a href="../logout.php" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
                 Logout
