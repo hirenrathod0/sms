2564da0b87c11aa487cb8b08ce135e6fc941b672
Parth <30324115+imparthpatel@users.noreply.github.com>
2020-02-27 23:37:55 +0530

    e

--- cmp_history.php
+++ cmp_history.php
@@ -35,7 +35,7 @@
 								</thead>
 								<tbody>
 									<?php $_SESSION['id']=2;
-									$query=mysqli_query($con,"select * from tblcomplaints where userId='".$_SESSION['id']."' ORDER BY regDate DESC");
+									$query=mysqli_query($con,"select * from tblcomplaints where userId='".$_SESSION['uid']."' ORDER BY regDate DESC");
 									while($row=mysqli_fetch_array($query))
 									{
 										?>
