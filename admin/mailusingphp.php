8084cdb71c702724654b8f28776065041e007d99
piyu872 <patelpriyanshi0807@gmail.com>
2020-02-28 02:39:11 +0530

    mail

--- admin/mailusingphp.php
+++ admin/mailusingphp.php
@@ -1,10 +1,36 @@
-<?php
 
-if(mail('bhaktisanjaybhai@gmail.com','test subject','Hello this is message','From:bhaktisanjaybhai@gmail'))
-{echo "sent";}
-else{
-echo "fail";}
-//mail("bhaktisanjaybhai@gmail.com","est subject","hello this is message");
-//echo "mailsent";  
+<?php include 'header.php';
+$query="select userEmail from users where id=2";
+$r=mysqli_query($con,$query);
+$row = mysqli_fetch_array($r);
+$to ='patelpriyanshi0807@gmail.com';
+$subject = 'maintenance bill';
+$from = 'ompriyanshipatel@gmail.com';
+ 
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ $query="select bill_date,due_date from maintenance_bill where bid='".$_REQUEST['bill_no']."'";
 
+$mtotal=$_REQUEST['total'];
+$r=mysqli_query($con,$query);
+$row = mysqli_fetch_array($r);
+// Compose a simple HTML email message
+$message = '<html><body>';
+$message .= '<h1 style="color:#f40;">bill date</h1>'.$row['bill_date'];
+$message .= '<p style="color:#080;font-size:18px;">due-date</p>'.$row['due_date'];
+$message .= '<p style="color:#080;font-size:18px;">total</p>'.$mtotal;
+$message .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
 ?>
--- admin/viewbill1.php
+++ admin/viewbill1.php
@@ -10,7 +10,7 @@
     <table class="table display" id="cattable" width="100%">
             <thead>
                 <!-- <tr style="text-align: center;"><th colspan="5"><h2>Flat Allotment List</h2></th></tr> -->
-                <th>flat no</th> <th>user name</th><th>contact no</th> <th>bill-date</th>  <th>total bill</th><th>due date</th> <th>view</th></thead>
+                <th>flat no</th> <th>user name</th><th>contact no</th> <th>bill-date</th>  <th>total bill</th><th>due date</th> <th>view</th><th>mail</th></thead>
             <tbody>
                 <?php 
                  $dog1=$_GET['uid'];
@@ -44,7 +44,8 @@
                               echo($total);?></td>
                                 <td><?php echo($row['due_date']);?></td>
                             
-                                <td><a href="bill_detail.php?bill_no=<?php echo $row['bid'];?>&total=<?php echo $total; ?>"  >Print</a></td>                                           
+                                <td><a href="bill_detail.php?bill_no=<?php echo $row['bid'];?>&total=<?php echo $total; ?>"  >Print</a></td> 
+                        <td><a href="mailusingphp.php?bill_no=<?php echo $row['bid'];?>&total=<?php echo $total; ?>"> email</a></td>
                         <!-- <td><a href="add_catspec.php?edit_cat=<?php //echo $rows['catid']; ?>" class="btn btn-info btn_space" >Edit</a><a href="add_catspec.php?delete_cat=<?php //echo $rows['catid']; ?>" onclick="return confirm('Are you sure?')" class="btn btn-danger" >Delete</a></td> -->
                     </tr>   
                     <?php 
