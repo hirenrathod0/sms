7fb71c5706203e0f755d19bc36d0dfb062d70c25
Parth <30324115+imparthpatel@users.noreply.github.com>
2020-02-28 07:39:36 +0530

    Update process.php

--- admin/process.php
+++ admin/process.php
@@ -64,7 +64,7 @@ $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
  
 // Create email headers
-$headers .= 'From: '.$from."\r\n".+
+$headers .= 'From: '.$from."\r\n".
     'Reply-To: '.$from."\r\n" .
     'X-Mailer: PHP/' . phpversion();
  
